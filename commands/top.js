9eaab0da6c24b863ea6ce7d2f7106580c04d3f41
Mafia-boss-lvl-420 <67177667+Mafia-boss-lvl-420@users.noreply.github.com>
2021-02-03 22:22:23 -0500

    Create top.js

---
+++ commands/top.js
@@ -0,0 +1,42 @@
+const fetch = require('node-fetch').default;
+module.exports = class {
+    constructor(bot) {
+        this.bot = bot;
+
+        this.name = 'top';
+        this.alli = [];
+
+        this.desc = 'See the top users!';
+
+        this.restCache = new Map();
+    }
+
+    async run(msg, args) {
+
+
+        let allUsers = await (await fetch(this.bot.baseLink + '/users?key=' + this.bot.config.API_KEY, {
+            method: 'GET',
+            headers: { auth: this.bot.config.API_ADMIN }
+        })).json();
+
+
+        allUsers = allUsers.sort((a, b) => b.stats.total-a.stats.total);
+        allUsers = allUsers.slice(0, 25);
+        let desc = '';
+        let countr = 1;
+        await allUsers.forEach(async user => {
+            if(user.stats.total == 0) return;
+            desc += `${countr}. <@!${user.id}>, ${user.stats.total}\n`;
+            countr++
+        });
+
+
+        desc = desc.slice(0, 2000);
+
+        msg.channel.createMessage({embed: {
+            title: 'Top users',
+            description: desc,
+            color: 0xf7c38e
+        }});
+    }
+}
